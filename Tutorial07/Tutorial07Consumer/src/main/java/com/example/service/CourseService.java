503b5da0a87df9d316a81587bcc041c28fae7f5f
anishaeva <anishaevaf@gmail.com>
2017-11-04 23:56:10 +0700

    hehe

---
+++ Tutorial07/Tutorial07Consumer/src/main/java/com/example/service/CourseService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+public interface CourseService
+{
+    CourseModel selectCourse (String id_course);
+    
+    List<CourseModel> selectAllCourse();
+}
